2a86b0459b795b305feb505ae22c927f4081c1f8
Tomi Tarvainen <ot_tomi@hotmail.com>
2023-11-09 17:58:54 +0200

    Add bases for pages

---
+++ Pages/Litters.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kissala.Pages
+{
+    public class LittersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
